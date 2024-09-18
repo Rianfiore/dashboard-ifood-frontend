41d23ab14725a9e51868238920d347c1a663d6a5
Tiago Ramos <tiagohenriqueramos@hotmail.com>
2024-09-17 23:19:17 -0300

    Atualização componente funcionario

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import { renderComponent } from "./utils/render-component.js";
-const header = renderComponent("./components/header");
-const sidebar = renderComponent("./components/sidebar");
-const dashboard = renderComponent("./screens/dashboard");
+//const header = renderComponent("./components/header");
+//const sidebar = renderComponent("./components/sidebar");
+//const dashboard = renderComponent("./screens/dashboard");
+const funcionario = renderComponent("./screens/funcionario");
 
-Promise.all([header, sidebar, dashboard]);
+
+Promise.all([funcionario]);
